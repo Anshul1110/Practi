a6de1a50022dbb2624bb9f387d25f0535deaf5c6
Anshul Verma <anshulverma96@gmail.com>
2017-03-28 05:24:38 +0530

    today's work

--- personal/hello.js
+++ personal/hello.js
@@ -3,3 +3,11 @@ function ansh(){
     document.getElementById('demo').innerHTML = Date();
 }
 
+function myFunction() {
+    var x = document.getElementById('modal');
+    if (x.style.display === 'none') {
+        x.style.display = 'flex';
+    } else {
+        x.style.display = 'none';
+    }
+}
