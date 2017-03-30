bfb37a4ee46fd1fea92fbb40c945fee439723560
Anshul Verma <anshulverma96@gmail.com>
2017-03-31 04:07:51 +0530

    introducing_javascript

--- personal/hello.js
+++ personal/hello.js
@@ -8,12 +8,15 @@ function myFunction() {
         x.style.display = 'none';
     }
 }
-function myFunction() {
-    var x = document.getElementById('home1');
-    if (x.style.display === 'none') {
-        x.style.display = 'flex';
-    } else {
-        x.style.display = 'none';
-    }
+function ansh(){
+    var z = document.getElementById('demo');
+   var x  = prompt("Do you love Anshul???", " ");
+   if(x==="yes"){
+       
+         var x=  alert('He also loves you alot and miss u alot!!');
+       
+   }
+else{
+  var x=  alert("Fuck off!!");
 }
-
+}
